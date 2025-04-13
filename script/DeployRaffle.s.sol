93d0f0c2e5a10f58c15cba3f4d71b69efb1fbe63
mfahad <sheikhshaikhfahad@gmail.com>
2025-04-13 22:03:01 +0500

    Created subscription for invalid consumer ID error

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script{
     function run() public{}
@@ -16,6 +17,8 @@ contract DeployRaffle is Script{
 
         if (config.subscriptionId == 0){
             // create a subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription.createSubscription(config.vrfCoordinator);
         }
 
         vm.startBroadcast();
