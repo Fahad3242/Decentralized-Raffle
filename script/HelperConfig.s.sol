c56ebb5283d7ef9c33a1cf6ef68c69efbe10baec
mfahad <sheikhshaikhfahad@gmail.com>
2025-04-19 00:12:59 +0500

    first execution completed

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -59,7 +59,7 @@ contract HelperConfig is CodeConstants, Script{
             interval: 30 seconds,
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             gaslane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-            subscriptionId: 0, 
+            subscriptionId: 43345989974817608451243104266155238485947286542505724122165566949174835814619, 
             callbackGasLimit: 500000,
             link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
         });
